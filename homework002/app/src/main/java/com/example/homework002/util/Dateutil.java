02502169feab68384a54657df81cc4f9156aff75
1123650393 <1123650393@qq.com>
2020-10-10 11:08:53 +0800

    2020智能手机第二次作业

---
+++ homework002/app/src/main/java/com/example/homework002/util/Dateutil.java
@@ -0,0 +1,18 @@
+package com.example.homework002.Util;
+
+import android.annotation.SuppressLint;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    public static String getNowDateTime(){
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddhhmmss");
+        return sdf.format(new Date());
+    }
+
+    public static String getNowTime() {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf=new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+}
