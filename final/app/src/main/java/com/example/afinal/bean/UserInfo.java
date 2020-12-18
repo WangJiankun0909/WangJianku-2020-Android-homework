f615b43ec5c378d83ea1d684cb5de41f3053b1f6
1123650393 <1123650393@qq.com>
2020-12-18 18:18:29 +0800

    期末大作业

---
+++ final/app/src/main/java/com/example/afinal/bean/UserInfo.java
@@ -0,0 +1,25 @@
+package com.example.afinal.bean;
+
+public class UserInfo {
+    public long rowid;
+    public int sn;
+    public String name;
+    public int age;
+    public boolean gender;
+    public boolean district;
+    public String update_time;
+    public String phone;
+    public String pwd;
+
+    public UserInfo() {
+        rowid = 0L;
+        sn = 0;
+        name = "";
+        age = 0;
+        gender = false;
+        district = false;
+        update_time = "";
+        phone = "";
+        pwd = "";
+    }
+}
